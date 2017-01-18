b50ea7d6a568d1ef2193e307fb2fe674a2b6ea30
aegis41 <garrrocks@gmail.com>
2017-01-17 20:45:15 -0500

    start add members feature

--- app/controllers/band.js
+++ app/controllers/band.js
@@ -2,13 +2,21 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
 	isEditing: false,
+	isAddingMembers: false,
 	actions: {
 		beginEditMode() {
 			this.set("isEditing", true);
 		},
+		beginAddMembersMode() {
+			this.set("isAddingMembers", true);
+		},
+		endAddMembersMode() {
+			this.toggleProperty("isAddingMembers");
+		},
 		save() {
 			this.get("model").save();
-			this.toggleProperty('isEditing');
+			this.toggleProperty("isEditing");
+			this.toggleProperty("isAddingMembers");
 		}
 	}
 });
